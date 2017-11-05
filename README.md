c26fc7e845b730f22109fb5a8a15f79bae97d578
Harvey Delaney <harvey.delaney@cba.com.au>
2017-11-05 22:50:21 +1100

    cleaned up code, fixed folder structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# Crypto Coaster 
+
+A quick little static web page that shows the famous Bitcoin mascot on his coaster ride of the price fluctuations.
+
+Using sockets from CryptoCompare (https://www.cryptocompare.com/) and simple CSS rotation depending on the % change.
+
+I like to think this project is simple - yet effective!
+
+## Live Page
+https://www.harveydelaney.com/crypto-coaster
--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,18 @@ class Constants {
     static get Red() {
         return 'rgb(120, 72, 61)';
     }
+
+    static get SocketUrl() {
+        return 'https://streamer.cryptocompare.com/';
+    }
+
+    static get UpArrow() {
+        return '&#8593;';
+    }
+
+    static get DownArrow() {
+        return '&#8595;';
+    }
 }
 
 class Coin {
@@ -17,6 +29,10 @@ class Coin {
         this.lastUpdated = 0;
         this.rotateValue = 90;
     }
+
+    selectCoin() {
+        $(`#${this.buttonId}`).addClass(`${this.selectedClass}`);
+    }
 }
 
 class Bitcoin extends Coin {
@@ -25,10 +41,6 @@ class Bitcoin extends Coin {
         this.buttonId = 'btc-button';
         this.selectedClass = 'selected-btc';
         this.imgUrl = 'assets/btc-coaster.gif';
-    }
-
-    selectCoin() {
-        $(`#${this.buttonId}`).addClass(`${this.selectedClass}`);
     }
 }
 
@@ -39,10 +51,6 @@ class Ether extends Coin {
         this.selectedClass = 'selected-eth';
         this.imgUrl = 'assets/eth-coaster.gif';
     }
-
-    selectCoin() {
-        $(`#${this.buttonId}`).addClass(`${this.selectedClass}`);
-    }
 }
 
 class Litecoin extends Coin {
@@ -51,10 +59,6 @@ class Litecoin extends Coin {
         this.buttonId = 'ltc-button';
         this.selectedClass = 'selected-ltc';
         this.imgUrl = 'assets/ltc-coaster.gif';
-    }
-
-    selectCoin() {
-        $(`#${this.buttonId}`).addClass(this.selectedClass);
     }
 }
 
@@ -93,16 +97,12 @@ class Litecoin extends Coin {
     }
 
     let initSocket = function() {
-        var socket = io.connect('https://streamer.cryptocompare.com/');
-        
-        //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
-        //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
-        //For aggregate quote updates use CCCAGG as market
+        var socket = io.connect(Constants.SocketUrl);
         var subscription = ['5~CCCAGG~BTC~USD','5~CCCAGG~ETH~USD', '5~CCCAGG~LTC~USD'];
         
-        socket.emit('SubAdd', {subs:subscription} );
+        socket.emit('SubAdd', { subs : subscription } );
         
-        socket.on("m", function(message){
+        socket.on("m", function (message){
             var messageType = message.substring(0, message.indexOf("~"));
             var res = {};
             if (messageType === CCC.STATIC.TYPE.CURRENTAGG) {
@@ -111,12 +111,6 @@ class Litecoin extends Coin {
             }						
         });
     } 
-
-    let initCoin = function(coin) {
-        $(`#${coin.buttonId}`).click(function() {
-            switchCoin(coin);
-        });
-    }
 
     let processUpdate = function(update) {
         let processCoin = coinDict[update.FROMSYMBOL];
@@ -133,10 +127,6 @@ class Litecoin extends Coin {
             }
 
             if (update.OPENHOUR){
-                // if (processCoin.yesterdayPrice === 0 ) {
-                //     initRotate(update.OPENHOUR);
-                // }
-
                 processCoin.yesterdayPrice = update.OPENHOUR;
             }
 
@@ -144,6 +134,12 @@ class Litecoin extends Coin {
                 updatePrice();
             }
         }
+    }
+
+    let initCoin = function(coin) {
+        $(`#${coin.buttonId}`).click(() => {
+            switchCoin(coin);
+        });
     }
 
     let switchCoin = function(coin) {
@@ -185,12 +181,16 @@ class Litecoin extends Coin {
 
         const yesterdayDelta = Math.round((currentCoin.price - currentCoin.yesterdayPrice) * 100) / 100;
         if (currentCoin.price >= currentCoin.yesterdayPrice) {
-            $('#yesterday-price').css('color', Constants.Green);
-            $('#yesterday-price').append(`(&#8593; $${Math.abs(yesterdayDelta)})`);
+            $('#yesterday-price')
+                .css('color', Constants.Green)
+                .append(`(${Constants.UpArrow} $${Math.abs(yesterdayDelta)})`);
+
             currentCoin.uptrend = true;
         } else {
-            $('#yesterday-price').css('color', Constants.Red);
-            $('#yesterday-price').append(`(&#8595; $${Math.abs(yesterdayDelta)})`);
+            $('#yesterday-price')
+                .css('color', Constants.Red)
+                .append(`(${Constants.DownArrow} $${Math.abs(yesterdayDelta)})`);
+
             currentCoin.uptrend = false;
         }
     }
@@ -214,10 +214,10 @@ class Litecoin extends Coin {
 
         changeDirection(currentCoin.rotateValue);
 
-        $('#delta').html(`(&#8593; $${delta})`);
+        $('#delta').html(`(${Constants.UpArrow} $${delta})`);
     }
     
-    let priceDecrease = function(delta) {
+    let priceDecrease = (delta) => {
         colorText(Constants.Red);
 
         const change = (delta / currentCoin.price) * 15000;
@@ -230,16 +230,16 @@ class Litecoin extends Coin {
 
         changeDirection(currentCoin.rotateValue);
 
-        $('#delta').html(`(&#8595; $${delta})`);
+        $('#delta').html(`(${Constants.DownArrow} $${delta})`);
     }
 
-    changeDirection = function(direction) {
+    let changeDirection = (direction) => {
         $('#coaster').css({'transform': 'rotate(' + direction + 'deg)'});
         $('#coaster').css({'-ms-transform': 'rotate(' + direction + 'deg)'});
         $('#coaster').css({'-webkit-transform': 'rotate(' + direction + 'deg)'});
     }
 
-    let colorText = function(rgb) {
+    let colorText = (rgb) => {
         $('#price').css('color', rgb);
         $('#delta').css('color', rgb);
     }
