789a2f75b4b766f231c58dd77f4bb372b5cd0963
Harvey Delaney <harvey@harveydelaney.com>
2020-04-19 13:35:03 +1000

    fix: update webpack for new CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,70 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let config = {
-	entry: './src/js/app.js',
-	output: {
-		filename: './bundle.js'
-	},
-	devServer: {
-		contentBase: './dist'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.(gif|png|jpe?g|svg)$/i,
-				use: [
-				  'file-loader',
-				]
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: [ 
-						{ 
-							loader: 'css-loader',
-							options: { minimize: true }
-						}, 
-						'sass-loader'
-					]
-				})
-			}
-		]
-	},
-	node: {
-		fs: "empty" // To get socket.io bundling correctly https://github.com/webpack-contrib/css-loader/issues/447
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery"
-		}),
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-			filename: 'index.html',
-			minify: {
-				removeAttributeQuotes: true,
-				collapseWhitespace: true,
-				html5: true,
-				minifyCSS: true,
-				removeComments: true,
-				removeEmptyAttributes: true,
-			},
-		}),
-		new ExtractTextPlugin("styles.css") /* extracts css to a separate file as opposed to inlining css */
-	]
-}
+  entry: "./src/js/app.js",
+  output: {
+    path: "build.js",
+    path: __dirname + "/build"
+  },
+  devServer: {
+    contentBase: "./build"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: { minimize: true }
+            },
+            "sass-loader"
+          ]
+        })
+      }
+    ]
+  },
+  node: {
+    fs: "empty" // To get socket.io bundling correctly https://github.com/webpack-contrib/css-loader/issues/447
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+      }
+    }),
+    new ExtractTextPlugin(
+      "styles.css"
+    ) /* extracts css to a separate file as opposed to inlining css */
+  ]
+};
 
 module.exports = config;
